f23cc273915da4b9e0fea1850afc7dc661876203
promlert pearrukkrai <promlertp@gmail.com>
2025-04-18 11:18:49 +0700

    fix Line Lift

--- src/Views/line/deposit.php
+++ src/Views/line/deposit.php
@@ -84,7 +84,7 @@
         var userProfile = "";
         $("#loading").show();
         document.addEventListener("DOMContentLoaded", function() {
-            liff.init({ liffId: '2005593789-x51eY5L0', withLoginOnExternalBrowser:true }).then(() => {
+            liff.init({ liffId: '2007230457-jPVJnZwg', withLoginOnExternalBrowser:true }).then(() => {
                 liff.getProfile().then(profile => {
                 userProfile = profile.userId;
                 const displayName = profile.displayName;
